3e455e278b0eded46530224c8083285eb361d322
Dixit <dixitk13@ccs.neu.edu>
2016-04-08 19:57:01 -0400

    adding mapper

--- CoolHadoop/src/coolmapreduce/Mapper.java
+++ CoolHadoop/src/coolmapreduce/Mapper.java
@@ -1,11 +1,18 @@
 package coolmapreduce;
 
-public abstract class Mapper {
+import java.io.IOException;
+
+public abstract class Mapper<KI, VI, KO, VO> {
+	
+	public void setup(Context context){
+		//noop??
+	}
 	
 	// TODO: set generics using input/output classes, and context
-	public void map(){};
-
+	public abstract void map(KI key, VI value, Context context) throws IOException, InterruptedException;
 	
-	
+	public void cleanup(Context context){
+		//noop??
+	}
 	
 }
