6c47c4c5e0c16c5e3d4de2dc27e67c6a26518418
Dixit <dixitk13@ccs.neu.edu>
2016-04-09 21:51:17 -0400

    addin WORKING MAPPER+FS

--- CoolHadoop/test/mapper/MapperTester.java
+++ CoolHadoop/test/mapper/MapperTester.java
@@ -1,8 +1,9 @@
-package mapper;
+package mapper;	
 
-import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import utils.Constants;
 import word.count.WordCount.TokenizerMapper;
@@ -11,22 +12,39 @@ import coolmapreduce.Job;
 import coolmapreduce.MapperHandler;
 
 public class MapperTester {
-
-	public static void main(String[] args) {
+	
+	public static String PATH = "C://Users//Dixit_Patel//Google Drive//Working on a dream//StartStudying//sem4//MapReduce//homeworks//hw8-Distributed Sorting//MR_Project//CoolHadoop//resources";
+	
+	public static List<String> getStupidFiles(){
+		List<String> files = new ArrayList<>();
+		files.add("alice.txt.gz");
 		
-		List<File> files = new ArrayList<>();
-		files.add(new File("alice.txt.gz"));
+		for(int i = 0 ; i< 3; i ++)
+			files.add("alice.txt.gz");
 		
-		for(int i = 0 ; i< 100; i ++)
-			files.add(new File("alice.txt.gz"));
+		return files;
+	}
+	
+	public static Map<Integer, List<String>> mimicMyParts(){
+		Map<Integer, List<String>> parts = new HashMap<>();
+		for(int i = 0 ; i < 3 ; i++)
+			parts.put(i, getStupidFiles());
+		
+		return parts;
+	}
 
+	public static void main(String[] args) throws Exception {
+		
+		List<String> files = getStupidFiles();
+		
+		System.out.println("here " + mimicMyParts());
+		
 		Configuration conf = new Configuration();
-		conf.set(Constants.INPUT_BUCKET_NAME, "resources");
+		conf.set(Constants.INPUT_BUCKET_NAME, PATH);
 		Job job = Job.getInstance(conf);
 		job.setMapperClass(TokenizerMapper.class);
 		MapperHandler mh = new MapperHandler(files, job);
-		mh.run();
-		
+		mh.runMapperHandler();		
 		
 	}
 }
